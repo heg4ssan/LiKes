37da860a3e00477c71121450c507fd79675614ce
Héctor Gassan <hectorgassansanchez@gmail.com>
2025-11-30 16:37:52 -0600

    se agrega comentarios y formato

--- Libro.h
+++ Libro.h
@@ -1,9 +1,25 @@
-#include <string>
+/*
+ * Proyecto LiKes
+ * Héctor Enrique Gassan Sanchez
+ * A017098097
+ * 30/11/2025
+ */
+
+/*
+ * Clase Libro con sus metodos
+ */
+
+#ifndef LIBRO_H_
+#define LIBRO_H_
+
 #include "AutorLibro.h"
+#include <string>
 using namespace std;
 
+// Declaracion de clase Libro
 class Libro {
   private:
+    // Declaro variables de instancia
     string titulo;
     string genero;
     AutorLibro autor;
@@ -11,10 +27,18 @@ class Libro {
     int paginas;
     float calificacion;
     int year;
+
   public:
-    Libro(): titulo(""), genero(""), autor(), editorial(""), paginas(0), calificacion(0.0), year(0){};
-    Libro(string t, string g, string n, int yn, int lb, string bi, string e, int p, float c, int y) :
-        titulo(t), genero(g), autor(n, yn, lb, bi), editorial(e), paginas(p), calificacion(c), year(y){};
+    // Constructor Vacio
+    Libro()
+        : titulo(""), genero(""), autor(), editorial(""), paginas(0),
+          calificacion(0.0), year(0) {};
+    // Constructor Parametrizado
+    Libro(string t, string g, string n, int yn, int lb, string bi, string e,
+          int p, float c, int y)
+        : titulo(t), genero(g), autor(n, yn, lb, bi), editorial(e), paginas(p),
+          calificacion(c), year(y) {};
+    // Declaro los metodos que tendra el objeto
     string getTitulo();
     string getGenero();
     AutorLibro getAutor();
@@ -32,58 +56,150 @@ class Libro {
     void setYear(int y);
 };
 
-string Libro :: getTitulo(){
-    return titulo;
+// Getters de lo atrubutos
+
+/**
+ * getter titulo
+ *
+ * @param
+ * @return string: titulo del libro
+ */
+string Libro ::getTitulo() { 
+  return titulo;
 }
 
-string Libro :: getGenero(){
-    return genero;
+/**
+ * getter genero
+ *
+ * @param
+ * @return string: genero del libro
+ */
+string Libro ::getGenero() { 
+  return genero;
 }
 
-AutorLibro Libro :: getAutor(){
-    return autor;
+/**
+ * getter nombre
+ *
+ * @param
+ * @return objeto tipo AutorLibro: autor del libro
+ */
+AutorLibro Libro ::getAutor() { 
+  return autor;
 }
 
-string Libro :: getEditorial(){
-    return editorial;
+/**
+ * getter editorial
+ *
+ * @param
+ * @return string: editorial del libro
+ */
+string Libro ::getEditorial() { 
+  return editorial;
 }
 
-int Libro :: getPaginas(){
-    return paginas;
+/**
+ * getter paginas
+ *
+ * @param
+ * @return int: numero de paginas del libro
+ */
+int Libro ::getPaginas() { 
+  return paginas;
 }
 
-float Libro :: getCalif(){
-    return calificacion;
+/**
+ * getter calificacion
+ *
+ * @param
+ * @return float: calificacion personal del libro
+ */
+float Libro ::getCalif() { 
+  return calificacion;
 }
 
-int Libro :: getYear(){
-    return year;
+/**
+ * getter year
+ *
+ * @param
+ * @return int: año de estreno del libro
+ */
+int Libro ::getYear() { 
+  return year;
 }
 
-void Libro :: setTitulo(string t) {
-    titulo = t;
+// Setters de los atrubutos
+
+/**
+ * setter de titulo
+ *
+ * Metodo que asigna la variable t
+ * a titulo
+ */
+void Libro ::setTitulo(string t) { 
+  titulo = t;
 }
 
-void Libro :: setGenero(string g) {
-    genero = g;
+/**
+ * setter de genero
+ *
+ * Metodo que asigna la variable g
+ * a genero
+ */
+void Libro ::setGenero(string g) { 
+  genero = g;
 }
 
-void Libro :: setAutor(string n, int yn, int lb, string bi) {
-    autor = AutorLibro(n, yn, lb, bi);
+/**
+ * setter de autor
+ *
+ * Metodo que recibe los atributos de la clase
+ * AutorLibro, reescribe el autor vacio con
+ * un autor contruido con sus parametros
+ *
+ */
+void Libro ::setAutor(string n, int yn, int lb, string bi) {
+  autor = AutorLibro(n, yn, lb, bi);
 }
 
-void Libro :: setEditorial(string e) {
-    editorial = e;
+/**
+ * setter de editorial
+ *
+ * Metodo que asigna la variable e
+ * a editorial
+ */
+void Libro ::setEditorial(string e) { 
+  editorial = e;
 }
 
-void Libro :: setPaginas(int p) {
-    paginas = p;
+/**
+ * setter de paginas
+ *
+ * Metodo que asigna la variable p
+ * a paginas
+ */
+void Libro ::setPaginas(int p) { 
+  paginas = p;
 }
 
-void Libro :: setCalificacion(float c) {
-    calificacion = c;
+/**
+ * setter de calificacion
+ *
+ * Metodo que asigna la variable c
+ * a calificacion
+ */
+void Libro ::setCalificacion(float c) { 
+  calificacion = c;
 }
 
-void Libro :: setYear(int y) {
-    year = y;
+/**
+ * setter de year
+ *
+ * Metodo que asigna la variable y
+ * a year
+ */
+void Libro ::setYear(int y) { 
+  year = y;
 }
+
+#endif // LIBRO_H_
