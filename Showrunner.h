c04c3805c22894180a2fa539a729069c0bb99071
Héctor Gassan <hectorgassansanchez@gmail.com>
2025-11-30 16:23:10 -0600

    agrego comentarios y estilo

--- Showrunner.h
+++ Showrunner.h
@@ -1,55 +1,116 @@
-#include<string>
+/*
+ * Proyecto LiKes
+ * Héctor Enrique Gassan Sanchez
+ * A017098097
+ * 30/10/2025
+ */
+
+/*
+ * Clase Showrunner con sus metodos
+ */
+
+#ifndef SHOWRUNNER_H_
+#define SHOWRUNNER_H_
+
+#include <string>
 using namespace std;
-class Showrunner{
+
+// Declaracion de clase Director
+class Showrunner {
   private:
-  string nombre;
-  int yearNacimiento;
-  int guionesEscritos;
-  string seriesAnteriores;
+    // Declaro variables de instancia
+    string nombre;
+    int yearNacimiento;
+    int guionesEscritos;
+    string seriesAnteriores;
 
   public:
-  Showrunner() : nombre(""), yearNacimiento(0), guionesEscritos(0), seriesAnteriores(""){};
-  Showrunner(string n, int yn, int g, string sa) : nombre(n), yearNacimiento(yn), guionesEscritos(g), seriesAnteriores(sa){};
-  string getNombre();
-  int getYearNacimiento();
-  int getGuiones();
-  string getSeries();
+    // Constructor vacio
+    Showrunner()
+        : nombre(""), yearNacimiento(0), guionesEscritos(0),
+          seriesAnteriores("") {};
+    // COnstructor parametrizado
+    Showrunner(string n, int yn, int g, string sa)
+        : nombre(n), yearNacimiento(yn), guionesEscritos(g),
+          seriesAnteriores(sa) {};
+    // Declaro los metodos que tendra el objeto
+    string getNombre();
+    int getYearNacimiento();
+    int getGuiones();
+    string getSeries();
 
-  void setNombre(string n);
-  void setYearNacimiento(int yn);
-  void setGuiones(int g);
-  void setSeries(string sa);
-
+    void setNombre(string n);
+    void setYearNacimiento(int yn);
+    void setGuiones(int g);
+    void setSeries(string sa);
 };
 
-string Showrunner :: getNombre(){
-    return nombre;
-}
+// Getters de los atributos
 
-int Showrunner :: getYearNacimiento(){
-    return yearNacimiento;
-}
+/**
+ * getter nombre
+ *
+ * @param
+ * @return string: nombre del director
+ */
+string Showrunner ::getNombre() { return nombre; }
 
-int Showrunner :: getGuiones(){
-    return guionesEscritos;
-}
+/**
+ * getter year nacimiento
+ *
+ * @param
+ * @return int: año de nacimiento del director
+ */
+int Showrunner ::getYearNacimiento() { return yearNacimiento; }
 
-string Showrunner :: getSeries(){
-    return seriesAnteriores;
-}
+/**
+ * getter guiones escritos
+ *
+ * @param
+ * @return int: numero de guiones escritos
+ */
+int Showrunner ::getGuiones() { return guionesEscritos; }
 
-void Showrunner :: setNombre(string n){
-    nombre = n;
-}
+/**
+ * getter series anteriores
+ *
+ * @param
+ * @return string: numero de series anteriores
+ */
+string Showrunner ::getSeries() { return seriesAnteriores; }
 
-void Showrunner :: setYearNacimiento(int yn){
-    yearNacimiento = yn;
-}
+// Setters de los atributos
 
-void Showrunner :: setGuiones(int g){
-    guionesEscritos = g;
-}
+/**
+ * setter de nombre
+ *
+ * Metodo que asigna la variable n
+ * a nombre
+ */
+void Showrunner ::setNombre(string n) { nombre = n; }
 
-void Showrunner :: setSeries(string sa){
-    seriesAnteriores = sa;
-}
+/**
+ * setter de year nacimiento
+ *
+ * Metodo que asigna la variable yn
+ * a yearNacimiento
+ */
+void Showrunner ::setYearNacimiento(int yn) { yearNacimiento = yn; }
+
+/**
+ * setter de guiones
+ *
+ * Metodo que asigna la variable g
+ * a guionesEscritos
+ */
+void Showrunner ::setGuiones(int g) { guionesEscritos = g; }
+
+/**
+ * setter de series anteriores
+ *
+ * Metodo que asigna la variable sa
+ * a seriesAnteriores
+ */
+void Showrunner ::setSeries(string sa) { seriesAnteriores = sa; }
+
+#endif // SHOWRUNNER_H_
