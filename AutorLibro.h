57febf68c3be81fbfc50b671f4cf9968ec98f0b1
Héctor Gassan <hectorgassansanchez@gmail.com>
2025-11-30 16:41:51 -0600

    se agrega comentarios y formato

--- AutorLibro.h
+++ AutorLibro.h
@@ -1,55 +1,130 @@
-#include<string>
+/*
+ * Proyecto LiKes
+ * Héctor Enrique Gassan Sanchez
+ * A017098097
+ * 30/11/2025
+ */
+
+/*
+ * Clase AutorLibro con sus metodos
+ */
+
+#ifndef AUTORLIBRO_H_
+#define AUTORLIBRO_H_
+
+#include <string>
 using namespace std;
-class AutorLibro{
+
+// Declaracion de clase Autorlibro
+class AutorLibro {
   private:
-  string nombre;
-  int yearNacimiento;
-  int librosPublicados;
-  string biografia;
+    // Declaro variables de instancia
+    string nombre;
+    int yearNacimiento;
+    int librosPublicados;
+    string biografia;
 
   public:
-  AutorLibro() : nombre(""), yearNacimiento(0), librosPublicados(0), biografia(""){};
-  AutorLibro(string n, int yn, int lb, string bi) : nombre(n), yearNacimiento(yn), librosPublicados(lb), biografia(bi){};
-  string getNombre();
-  int getYearNacimiento();
-  int getLibros();
-  string getBiografia();
+    // Constructor vacio
+    AutorLibro()
+        : nombre(""), yearNacimiento(0), librosPublicados(0), biografia("") {};
+    // Constructor parametrizado
+    AutorLibro(string n, int yn, int lb, string bi)
+        : nombre(n), yearNacimiento(yn), librosPublicados(lb), biografia(bi) {};
+    // Declaro los metodos que tendra el objeto
+    string getNombre();
+    int getYearNacimiento();
+    int getLibros();
+    string getBiografia();
 
-  void setNombre(string n);
-  void setYearNacimiento(int yn);
-  void setLibros(int lb);
-  void setBiografia(string bi);
-
+    void setNombre(string n);
+    void setYearNacimiento(int yn);
+    void setLibros(int lb);
+    void setBiografia(string bi);
 };
 
-string AutorLibro :: getNombre(){
-    return nombre;
+// Getters de los atributos
+
+/**
+ * getter nombre
+ *
+ * @param
+ * @return string: nombre del autor
+ */
+string AutorLibro ::getNombre() { 
+  return nombre;
 }
 
-int AutorLibro :: getYearNacimiento(){
-    return yearNacimiento;
+/**
+ * getter year nacimiento
+ *
+ * @param
+ * @return int: año de nacimiento del autor
+ */
+int AutorLibro ::getYearNacimiento() { 
+  return yearNacimiento;
 }
 
-int AutorLibro :: getLibros(){
-    return librosPublicados;
+/**
+ * getter libros publicados
+ *
+ * @param
+ * @return int: numero de libros publicados
+ */
+int AutorLibro ::getLibros() { 
+  return librosPublicados;
 }
 
-string AutorLibro :: getBiografia(){
-    return biografia;
+/**
+ * getter biografia
+ *
+ * @param
+ * @return string: biografia del autor
+ */
+string AutorLibro ::getBiografia() { 
+  return biografia;
 }
 
-void AutorLibro :: setNombre(string n){
-    nombre = n;
+// Setters de los atributos
+
+/**
+ * setter de nombre
+ *
+ * Metodo que asigna la variable n
+ * a nombre
+ */
+void AutorLibro ::setNombre(string n) { 
+  nombre = n;
 }
 
-void AutorLibro :: setYearNacimiento(int yn){
-    yearNacimiento = yn;
+/**
+ * setter de year nacimiento
+ *
+ * Metodo que asigna la variable yn
+ * a yearNacimiento
+ */
+void AutorLibro ::setYearNacimiento(int yn) { 
+  yearNacimiento = yn;
 }
 
-void AutorLibro :: setLibros(int lb){
-    librosPublicados = lb;
+/**
+ * setter de libros publicados
+ *
+ * Metodo que asigna la variable lb
+ * a librosPublicados
+ */
+void AutorLibro ::setLibros(int lb) { 
+  librosPublicados = lb;
 }
 
-void AutorLibro :: setBiografia(string bi){
-    biografia = bi;
+/**
+ * setter de biografia
+ *
+ * Metodo que asigna la variable bi
+ * a biografia
+ */
+void AutorLibro ::setBiografia(string bi) { 
+  biografia = bi;
 }
+
+#endif // AUTORLIBRO_H_
